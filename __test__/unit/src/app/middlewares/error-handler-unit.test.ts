beb5edda07c10ff7209647221a8ab30c7d882834
Pablo ruan dos santos <pablords@gmail.com>
2022-07-05 23:12:04 -0300

    feat: updated error-handler unit tests

--- __test__/unit/src/app/middlewares/error-handler-unit.test.ts
+++ __test__/unit/src/app/middlewares/error-handler-unit.test.ts
@@ -15,6 +15,12 @@ describe("Testes unitarios de middlewares", () => {
     expect(resError).toBe(true)
   })
 
+  it("Deve chamar a funcao isOperationalError com instancia de APIError e retornar false error operacional", async () => {
+    const error = new Error()
+    const resError = await errorHandlerMiddleware.isOperationalError(error)
+    expect(resError).toBe(false)
+  })
+
   it("Deve chamar a funcao logError com instancia de APIError", async () => {
     const spy = jest.spyOn(errorHandlerMiddleware, "logError")
     const error = new APIError("UNPROCESSABLE_ENTITY",
@@ -65,5 +71,16 @@ describe("Testes unitarios de middlewares", () => {
     const called = await errorHandlerMiddleware.returnError(error, req, res, next)
     expect(spy).toHaveBeenCalled()
     expect(called).toBe(undefined)
+  })
+
+  it("deve passar pelo middleware e chamar funcao next caso nao seja passado um error", async () => {
+    const req = { body: {} } as any
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn()
+    } as any
+    const next = jest.fn()
+    await errorHandlerMiddleware.logErrorMiddleware(null, req, res, next)
+    expect(next).toBeCalledTimes(2)
   })
 })
